97259d484b7cc636c1f46e0e68f4df456949e040
Kuba <staniojakub@gmail.com>
2022-04-24 22:31:47 +0300

    refactor(shell): change wording for micro frontend statuses

--- apps/shell/src/app/services/mfe-hpcheck/mfe-hpcheck.service.ts
+++ apps/shell/src/app/services/mfe-hpcheck/mfe-hpcheck.service.ts
@@ -11,12 +11,10 @@ export class MfeHpCheckService {
 
   hpCheckTodo() {
     return this.isUrlUp(`${env.mfe.todo}/remoteEntry.mjs`);
-    // return this.isUrlUp(`${env.mfe.todo}/remoteEntry.js`);
   }
 
   hpCheckSettings() {
     return this.isUrlUp(`${env.mfe.settings}/remoteEntry.mjs`);
-    // return this.isUrlUp(`${env.mfe.settings}/remoteEntry.js`);
   }
 
   isUrlUp(url: string) {
