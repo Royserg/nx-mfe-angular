9fed90599be89ebcd7334fd5a49fde659dd331ed
Kuba <staniojakub@gmail.com>
2022-05-03 00:05:52 +0300

    refactor(settings): add placeholder data to view

--- apps/settings/src/app/remote-entry/entry.component.ts
+++ apps/settings/src/app/remote-entry/entry.component.ts
@@ -1,18 +1,28 @@
+import { UserQuery } from '@angular-mfe/shared/store';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'angular-mfe-settings-entry',
-  template: `<div class="remote-entry">
-    <h2>Settings Component</h2>
-  </div>`,
+  template: `
+    <h2 class="mb-1">Settings</h2>
+
+    <p class="mb-1">Placeholder Settings Micro Frontend</p>
+    <h3 class="user-details">Username: {{ username$ | async }}</h3>
+  `,
   styles: [
     `
-      .remote-entry {
-        background-color: #143055;
-        color: white;
-        padding: 5px;
+      .mb-1 {
+        margin-bottom: 1rem;
+      }
+
+      .user-details {
+        border: 1px solid orangered;
       }
     `,
   ],
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent {
+  username$ = this.userQuery.select('name');
+
+  constructor(private userQuery: UserQuery) {}
+}
